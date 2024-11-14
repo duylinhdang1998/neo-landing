55188c6990db2fb9d41188e3248ca2909497aca1
Dang Duy Linh <duylinhdang1998@gmail.com>
2024-11-15 01:34:56 +0700

    fix ssr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+"use client"
 import CoreModules from '@/components/CoreModules/CoreModules';
 import SectionConverted from '@/components/SectionConverted/SectionConverted';
-import SectionHero from '@/components/SectionHero/SectionHero';
+// import SectionHero from '@/components/SectionHero/SectionHero';
 import SectionMarket from '@/components/SectionMarket/SectionMarket';
 import SectionTimeLine from '@/components/Timeline/SectionTimeline';
+import dynamic from 'next/dynamic';
+
+const SectionHero = dynamic(
+  () => import('@/components/SectionHero/SectionHero'),
+  { ssr: false }
+);
 
 export default function Home() {
   return (
